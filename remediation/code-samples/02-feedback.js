48e2bc19626477261f3ea9df616f181057b68dff
ted <ted@warpedpuppy.com>
2020-05-07 16:46:44 -0400

    create feedback for assignments

---
+++ remediation/code-samples/02-feedback.js
@@ -0,0 +1,26 @@
+/*
+feedback for 02-countValue.js:
+
+1) Your testing function
+
+Please see the feed back in 01-feedback.js regarding your testing function.
+
+2) For loops 
+
+Please see the feed back in 01-feedback.js regarding for loops.
+
+3) Loose equality
+
+Please see the feedback in 01-feedback.js on loose equality.js.
+
+4) Code formatting / consistency
+
+Please see the feedback in 01-feedback.js on code formatting and consistency.
+
+5) Quick nudge in the right direction:
+
+JavaScript has a bunch of array methods that are incredibly useful.  You tried 'splice' this time, but if it were me, I'd go back to the source and maybe checkout the 'filter' method for this scenario.
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/filter
+
+*/
